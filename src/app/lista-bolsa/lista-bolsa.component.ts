d546d517eed458d5be0d061dd9020701496d6ba8
HoracioFerG <horaciofernandezgar@gmail.com>
2023-06-12 15:42:12 -0600

    Id agregado a tabla de bolsa

--- src/app/lista-bolsa/lista-bolsa.component.ts
+++ src/app/lista-bolsa/lista-bolsa.component.ts
@@ -14,33 +14,51 @@ export class ListaBolsaComponent implements OnInit {
   listaBolsa: Array<Object> = [];
   idNivel = '0';
   prevHeight = $('html, body').height();
+  $tableElement = $('.tableContainer');
 
   getBolsaPorNivel(idNivel) {
     if (idNivel === 0) {
       return;
     }
+    console.log(this.$tableElement.prop('scrollHeight'));
+    console.log(this.$tableElement);
+    this.stopScroll();
     this.service.getBolsaDisponibleByNivel(idNivel).subscribe((data: any) => {
-      this.stopScroll();
-
       this.listaBolsa = data.bolsa;
-
-      // this.prevHeight = $('html, body').height();
-
-      this.scrollDown(this.prevHeight);
+      this.tableScroll();
     });
   }
 
   scrollState = 'top';
 
-  ngAfterViewChecked(): void {
-    this.scrollDown(this.prevHeight);
-  }
+  ngAfterViewChecked(): void {}
   ngOnInit(): void {}
 
+  tableScroll() {
+    const tableContainer = document.getElementById('tableContainer');
+    tableContainer.scrollTop = tableContainer.scrollTop + 1;
+    $('p:nth-of-type(1)').html('scrollTop : ' + tableContainer.scrollTop);
+    $('p:nth-of-type(2)').html('scrollHeight : ' + tableContainer.scrollHeight);
+    if (tableContainer.scrollTop == tableContainer.scrollHeight - 100) {
+      tableContainer.scrollTop = 0;
+    }
+    setTimeout(this.tableScroll, 25);
+  }
+
+  animateTable() {
+    const st = this.$tableElement.scrollTop();
+    const sb =
+      this.$tableElement.prop('scrollHeight') -
+      this.$tableElement.innerHeight();
+    this.$tableElement.animate(
+      { scrollTop: st < sb / 2 ? sb : 0 },
+      4000,
+      this.animateTable
+    );
+  }
+
   stopScroll() {
-    $('html, body').animate({
-      scrollTop: 0,
-    });
+    this.$tableElement.stop();
   }
 
   scrollDown(height) {
